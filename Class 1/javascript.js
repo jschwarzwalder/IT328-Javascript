2d6898432c5d236eec635aafafefe471e0919042
Jami Schwarzwalder <jamischwarzwalder@gmail.com>
2016-03-29 20:00:31 -0700

    Creating a separate JavaScript file

---
+++ Class 1/javascript.js
@@ -0,0 +1,7 @@
+
+function bold(){
+	document.getElementById("games").innerHTML = "Awesome games you should play!";
+}
+//change the <p> tag
+
+document.getElementsByTagName('li').style.Color = "Navy";
