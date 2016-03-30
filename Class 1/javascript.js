83a427deeecf5ed037a71a4af0142f40c5ec7743
Jami Schwarzwalder <jamischwarzwalder@gmail.com>
2016-03-29 20:48:56 -0700

    Creating a JS object

--- Class 1/javascript.js
+++ Class 1/javascript.js
@@ -8,8 +8,7 @@ function foo() {
 	 document.write(i)
 }
 //document.getElementsByTagName('li').style.color = 'white'; // doesn't work I don't know why
-
-document.getElementById('games').style.color = 'navy'; //Works
+//document.getElementById('games').style.color = 'navy'; //Works
 
 var myFoo = function() {
 	alert("Hello World!");
@@ -28,7 +27,23 @@ document.write(typeof myFoo); //function
 document.write("<br />");
 document.write(typeof [1, 2, 3, 4]); //the type of arrays in object in JS
 document.write("<br />");
-document.write(typeof {key: value});
+document.write(typeof {key: "value"});
 document.write("<br />");
-document.write({showName: "Orphan Black", year: "2013");
+document.write({showName: "Orphan Black", year: "2013"});
+document.write("<br />");
+
+
+//create a useful JS object
+var book = {
+	author: "Pablo Coelho",
+	title: "The Alchemist", 
+	publisher: "Harper Torch",
+	genres: ["quest", "survival", "fantasy", "drama"],
+	synopsis: "Quest and stuff..."
+};
+
+document.write("<br />");
+document.write("Author: " + book.author);
+document.write("<br />");
+document.write("Title: " + book.title);
 document.write("<br />");
