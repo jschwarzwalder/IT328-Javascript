844789138ddd85248114faa23215155fe0d13ae3
Jami Schwarzwalder <jamischwarzwalder@gmail.com>
2016-03-29 20:40:43 -0700

    type of

--- Class 1/javascript.js
+++ Class 1/javascript.js
@@ -7,7 +7,7 @@ function foo() {
 	 i = 10
 	 document.write(i)
 }
-document.getElementsByTagName('li').style.color = 'white'; // doesn't work I don't know why
+//document.getElementsByTagName('li').style.color = 'white'; // doesn't work I don't know why
 
 document.getElementById('games').style.color = 'navy'; //Works
 
@@ -15,4 +15,20 @@ var myFoo = function() {
 	alert("Hello World!");
 }
 
-myFoo();
+//myFoo();
+document.write(typeof "John"); //string
+document.write("<br />");
+document.write(typeof 100.0); //number
+document.write("<br />");
+document.write(typeof false); //boolean
+document.write("<br />");
+document.write(typeof foo); //fuction
+document.write("<br />");
+document.write(typeof myFoo); //function
+document.write("<br />");
+document.write(typeof [1, 2, 3, 4]); //the type of arrays in object in JS
+document.write("<br />");
+document.write(typeof {key: value});
+document.write("<br />");
+document.write({showName: "Orphan Black", year: "2013");
+document.write("<br />");
