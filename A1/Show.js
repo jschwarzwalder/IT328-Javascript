0d65942054947d70fc6dd8362c56e4b655b13698
Jami <jamischwarzwalder@gmail.com>
2016-03-31 00:18:33 -0700

    adding Harkness

--- A1/Show.js
+++ A1/Show.js
@@ -2,7 +2,7 @@ var show = {
 name:"Doctor Who", 
 synopsis:"An alien travels around space and time with a companion, and occasionally saves the Earth. He really likes Great Britain, so many companions and adventures center around that country.", 
 year: 1963,
-watch: [“BBC”, “BBC America”, “Amazon”, “Hulu”, “Netflix”, “Syfy Universal”],
+watch: ["BBC", "BBC America", "Amazon", "Hulu", "Netflix", "Syfy Universal"],
 Actors: { doctor: ["John Hurt", "William Hartnell", "Patrick Troughton", "Jon Pertwee", "Tom Baker", "Peter Davison", "Colin Baker", "Sylvester McCoy", "Paul McGann", "Christopher Eccleston", "David Tennant", "Matt Smith", "Peter Capaldi" ], 
-companion: ["Rose”, "Mickey", "Jackie", "Martha”, "Donna", "Amy Pond", "Rory", "River Song", "Clara Oswald", ];
+companion: ["Rose", "Mickey", "Jackie", "Captain Jack Harkness" "Martha", "Donna", "Amy Pond", "Rory", "River Song", "Clara Oswald", ];
 }
